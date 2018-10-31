e1e266dd5bf0523a4a680b5f7802c983f5da5199
TonyGregg <antonygenil@gmail.com>
2018-10-30 21:41:26 -0500

    Cleaned up the code a bit..Ready to add login logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import {AppComponent} from './app.component';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
-import {PolicyviewComponent} from './components/policyview/policyview.component';
 import {AboutComponent} from './components/about/about.component';
 import {ContactComponent} from './components/contact/contact.component';
-import {HeaderComponent} from './ui/header/header.component';
 import {NgModule} from '@angular/core';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {HomeComponent} from './home/home.component';
 
 
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent},
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'aboutus', component: AboutComponent },
