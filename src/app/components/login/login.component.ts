a255d584a3deba7458daf1fb5ed45c7a232d3791
TonyGregg <antonygenil@gmail.com>
2018-11-03 16:26:00 -0500

    Login form validation completed. Need to integrate with backend rest API is pending.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import {Router} from '@angular/router';
 import {User} from '../../model/user';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,9 @@ import {User} from '../../model/user';
 })
 export class LoginComponent implements OnInit {
   user = new User();
+  userId = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required, Validators.minLength(5)]);
+
 
   constructor(
     private router: Router
@@ -22,7 +26,15 @@ export class LoginComponent implements OnInit {
   // convenience getter for easy access to form fields
   // get f() { return this.loginForm.controls; }
 
-  tryLogin() {
+  getUserIdError() {
+    return this.userId.hasError('required') ? 'You must enter a value' : '';
+  }
+  getPasswordError() {
+    return this.password.hasError('required') ? 'You must enter a value' :
+        '';
+  }
+
+  onFormSubmit() {
     console.log('Submitted login ' + JSON.stringify(this.user));
     this.router.navigateByUrl('/policy');
   }
