890be6e230d2f1d821bcbb7b561b78473f2dc151
TonyGregg <antonygenil@gmail.com>
2018-11-03 00:26:37 -0500

    quick validation added on email

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../model/user';
 import {Router} from '@angular/router';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,6 +10,14 @@ import {Router} from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
   user = new User();
+  email = new FormControl('', [Validators.required, Validators.email]);
+  private emailMsg: string;
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
+  }
 
   constructor(  private router: Router
 
@@ -21,6 +30,13 @@ export class RegisterComponent implements OnInit {
 
 
   registerUser() {
+    this.emailMsg = this.getErrorMessage();
+    if(!(this.emailMsg === '')) {
+      console.log('not a valid email');
+
+      return;
+    }
+    this.user.email = this.email.value;
     console.log('Submitted login ' + JSON.stringify(this.user));
 
   }
